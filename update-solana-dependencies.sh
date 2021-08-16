e642f260139e9987cf3cc065f59ff52504c98b04
Michael Vines <mvines@gmail.com>
2021-08-15 22:05:24 -0700

    Update update-solana-dependencies.sh

--- update-solana-dependencies.sh
+++ update-solana-dependencies.sh
@@ -3,13 +3,27 @@
 # Updates the solana version in all the SPL crates
 #
 
+here="$(dirname "$0")"
+
 solana_ver=$1
 if [[ -z $solana_ver ]]; then
   echo "Usage: $0 <new-solana-version>"
   exit 1
 fi
 
-cd "$(dirname "$0")"
+if [[ $solana_ver =~ ^v ]]; then
+  # Drop `v` from v1.2.3...
+  solana_ver=${solana_ver:1}
+fi
+
+cd "$here"
+
+echo "Updating Solana version to $solana_ver in $PWD"
+
+if ! git diff --quiet && [[ -z $DIRTY_OK ]]; then
+  echo "Error: dirty tree"
+  exit 1
+fi
 
 declare tomls=()
 while IFS='' read -r line; do tomls+=("$line"); done < <(find . -name Cargo.toml)
@@ -34,10 +48,10 @@ crates=(
   solana-stake-program
   solana-transaction-status
   solana-validator
-  solana-vote-program
 )
 
 set -x
 for crate in "${crates[@]}"; do
-  sed -i -e "s#\(${crate} = \"\)\(=\?\).*\(\"\)#\1\2$solana_ver\3#g" "${tomls[@]}"
+  sed -i -e "s#\(${crate} = \"\).*\(\"\)#\1=$solana_ver\2#g" "${tomls[@]}"
 done
+
